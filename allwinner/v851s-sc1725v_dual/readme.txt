9d433a9a7cdc452593c2adc9c44e00ee9d0c5799
Snake <snake@example.com>
2023-03-21 09:36:42 +0800

    add 851s sc1725v dual camera
    Change-Id: I5c4fcc5926f6be680e4b6ab2525c5f1519461c96

---
+++ allwinner/v851s-sc1725v_dual/readme.txt
@@ -0,0 +1,11 @@
+adb shell
+
+     
+1、wifi
+      wifi -o sta
+      wifi -c abcde 19891206
+      ifconfig  可以看到分配的IP地址
+      ping www.baidu.com 可以看到连通
+
+2、蓝牙
+     手机端安装BLE调试助手。可以搜索到[aw-ble-test-007]类似的ble蓝牙
---
+++ allwinner/v851s-sc1725v_dual/v851s_sc1725v_dual.mk
@@ -0,0 +1,14 @@
+$(call inherit-product-if-exists, target/allwinner/v851s-common/v851s-common.mk)
+
+PRODUCT_PACKAGES +=
+
+PRODUCT_COPY_FILES +=
+
+PRODUCT_AAPT_CONFIG := large xlarge hdpi xhdpi
+PRODUCT_AAPT_PERF_CONFIG := xhdpi
+PRODUCT_CHARACTERISTICS := musicbox
+
+PRODUCT_BRAND := allwinner
+PRODUCT_NAME := v851s_sc1725v_dual
+PRODUCT_DEVICE := v851s-sc1725v_dual
+PRODUCT_MODEL := Allwinner v851s sc1725v dual board
